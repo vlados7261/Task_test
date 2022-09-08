3482b402abadd68f20668c6168d9642260539367
Владислав Левшин <levshin.va18@physics.msu.ru>
2022-09-08 18:16:44 +0300

    Добавление комментариев

--- testing.py
+++ testing.py
@@ -3,21 +3,32 @@ import numpy as np
 from transformers import pipeline
 url="https://drive.google.com/file/d/17_HRkT6T6LnGHYDDKG-NNDPenTW-vMbC/view?usp=sharing"
 url='https://drive.google.com/uc?id='+url.split('/')[-2]
+
+## Скачивание датасета с гугл диска
+
 DATA = pd.read_csv(url)
 kol_dial=np.array(DATA['dlg_id'])[-1]
 dialogs= [ DATA[DATA['dlg_id']==i] for i in range(kol_dial+1) ]
+
+## Выбор НС и режима ее работы
+
 p = pipeline(
   task='zero-shot-classification', 
   model='cointegrated/rubert-base-cased-nli-twoway'
 )
 
-
-
+## Функция для обработки 1 предложения:
+## предложение режется по словам и получаем ответ от НС от каждого кусочка
+##
 
 def sent_processing(sent,param):
   #plt.figure(figsize=(40,20))
   result=[]
   data=[ sent.split(' ')[:i+1] for i in range(len(sent.split(' '))) ]
+
+  ## Actions - набор "смысловых нагрузок в предложении" ,
+  # как результат работы НС получаем результат по каждому "смыслу"
+
   if len(data)>3:
     actions=['здравствуйте', 'до свидания', 'имя', 'название компании']
   else:
@@ -48,34 +59,34 @@ def sent_processing(sent,param):
 
 
 i=0
-RESULTS=[]
+RESULTS=[]  # Список ответов для каждого предложения
+
+
+
 for dil in dialogs:
   for row in np.array(dil):
     result=[]
     if row[2]=='manager':
       sent=row[3]
       result.append(sent_processing(sent,param=0.85))
-      '''print(sent)
-      for act in actions:
-        answer=p(    sequences=sent, 
-                        candidate_labels=act, 
-                        hypothesis_template="{}.")['scores']
-        if answer[0]>0.6:
-          print(answer)
-          result.append(act)
-      print(result)
-    time.sleep(0.1)'''
     RESULTS.append(result)
       #print(actions[answer.index(max(answer))])
 
 
-DATA['ВЫВОД']=RESULTS
+DATA['ВЫВОД']=RESULTS   # Добавления столбца с результатами в исходную таблицу
+
+
 
 name=f"Таблица с результатами.xlsx"
-print(name)
+
+# Сохранение полученно таблицы в Excel формате
+
 DATA.to_excel(name)
 print('По результатам анализа: ')
 ind=0
+
+# Проверка условия, что менеджер поздоровался и попрощался
+
 for n_d in range(len(dialogs)):
   privet=False
   dosvid=False
